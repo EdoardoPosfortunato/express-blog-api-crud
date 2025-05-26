2260bf5819497feb72347878176e743202686fef
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-26 18:03:40 +0200

    feat: bonus

--- controllers/seriecontroller.js
+++ controllers/seriecontroller.js
@@ -3,48 +3,77 @@ import serieComiche from "../tvSeries.js"
 
 // INDEX
 const index = (req, res) => {
-  res.json({
-    data: serieComiche,
-    count: serieComiche.length,
-  });
+
+    const genereFilter = req.query.genere;
+
+    let result = serieComiche;
+
+    if (genereFilter !== undefined) {
+
+        result = serieComiche.filter((curSerie) =>
+            curSerie.genere.includes(genereFilter)
+        );
+
+    }
+
+    res.json({
+        data: result,
+        count: result.length,
+    });
 }
 
 // SHOW
 const show = (req, res) => {
-  const serieID = parseInt(req.params.id);
-  const serie = serieComiche.find((curSerie) => curSerie.id == serieID);
-  res.json({
-    data: serie
-  });
+    const serieID = parseInt(req.params.id);
+    const serie = serieComiche.find((curSerie) => curSerie.id == serieID);
+    res.json({
+        data: serie
+    });
 }
 
 //STORE
 const store = (req, res) => {
-  res.json({
-    data: "Aggiungo una serie",
-  });
+    res.json({
+        data: "Aggiungo una serie",
+    });
 }
 
 //UPDATE
 const update = (req, res) => {
-  const seriID = req.params.id;
-  res.json({
-    data: `Modifico un singolo film con id ${seriID}`,
-  });
+    const serieID = req.params.id;
+    res.json({
+        data: `Modifico un singolo film con id ${serieID}`,
+    });
 }
 
 //DESTROY 
 const destroy = (req, res) => {
 
-  const seriID = req.params.id;
-  
-  res.status(202)
-  
-  res.json({
-    data: `Cancello un film con id ${seriID}`,
-  });
+    const serieID = req.params.id;
 
-  
+    console.log(serieID)
+
+    const index = serieComiche.findIndex((curSerie) => curSerie.id === serieID)
+
+    // console.log(serieComiche)
+
+    if (index === -1) {
+        res.status(404);
+        return res.json({
+            error: "Serie non trovata",
+        });
+    }
+
+
+    const eliminata = serieComiche.splice(index, 1);
+
+    console.log(serieComiche)
+
+    res.status(200).json({
+        commento: "La serie eliminata è",
+        data: eliminata
+    })
+
 }
 
 
